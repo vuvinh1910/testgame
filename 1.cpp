bad542b7f94d7e418d59d8ce20c459e5166037e0
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-27 12:10:16 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,26 +1,40 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() {
-    int v;
-    cin >> v;
-    while (v--) {
-        long long a;
-        cin >> a;
-        int u=2; int o=0;
-        while(a!=1){
-            if(a%u==0 ){
-                a=a/u;
-                o++;
-            } else {
-                if(o!=0 || a==1) {cout << u <<" "<< o<< " ";}
-                o=0;
-                u++;
-                }
-            if (a==1){if(o!=0) {cout << u <<" "<< o<< "\n";}}
+bool check(vector<int>& vc){
+    for (int i = 0; i < vc.size(); i++)
+    {
+        for (int j = i+1; j < vc.size(); j++){
+            if(vc[i]==vc[j]) return false;
         }
+        
     }
+    return true;
+}
 
+int main(){
+    int t; cin >> t;
+    while (t--)
+    {
+        int a; cin >>a;
+        int i=2; int dem=0;
+        vector<int> vc;
+        while (a>1)
+        {
+            if(a%i==0){
+                dem++;
+                vc.push_back(i);
+                a=a/i;
+            } else {
+                i++;
+            }
+        }
+
+        if(dem==3){
+            if(check(vc)){cout <<"1\n";}
+            else{cout << "0\n";}
+        } else cout << "0\n";
+    }
+    
     return 0;
 }
