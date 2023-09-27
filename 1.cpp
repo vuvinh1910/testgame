03fb6638619aa56d3699fe864735232107ab0264
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-09-27 12:11:59 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,39 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check(vector<int>& vc){
-    for (int i = 0; i < vc.size(); i++)
-    {
-        for (int j = i+1; j < vc.size(); j++){
-            if(vc[i]==vc[j]) return false;
-        }
-        
-    }
-    return true;
-}
-
 int main(){
     int t; cin >> t;
     while (t--)
     {
-        int a; cin >>a;
-        int i=2; int dem=0;
-        vector<int> vc;
-        while (a>1)
-        {
-            if(a%i==0){
-                dem++;
-                vc.push_back(i);
-                a=a/i;
-            } else {
-                i++;
-            }
+        int a,b; cin >>a>>b;
+        int c,d; cin >>c>>d;
+        int count=0;
+        if(a>b){
+            int temp = a;
+            a=b;
+            b=temp;
         }
-
-        if(dem==3){
-            if(check(vc)){cout <<"1\n";}
-            else{cout << "0\n";}
-        } else cout << "0\n";
+        for (int i = a; i <= b; i++)
+        {
+            if(i%c==0) {count++;}
+            else if(i%d==0) {count++;}
+        }
+        cout << count <<endl;
+        
     }
     
     return 0;
